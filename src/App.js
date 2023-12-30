5efb71b528450db7a1ea3f25680b8390b610f478
jitu <jitukumar020897@gmail.com>
2023-12-30 17:13:26 +0530

    setup react project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Menu from "./function/Menus";
 import Home from "./function/Home";
 import About from "./function/About";
@@ -8,103 +8,24 @@ import Footer from "./function/Footer";
 import Servies from './function/Servies';
 import Head from "./function/Head";
 
- function App(){
-    return(
+function App() {
+    return (
         <>
 
 
-<BrowserRouter>
-<Head/>
-<Menu/>
-<Routes>
-<Route path="/" element={<Home/>}/>
-<Route path="/about" element={<About/>}/>
-<Route path="/servies" element={<Servies/>}/>
-<Route path="/contact" element={<Contact/>}/>
-</Routes>
-<Footer/>
-</BrowserRouter>
+            <BrowserRouter>
+                <Head />
+                <Menu />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/servies" element={<Servies />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
         </>
     );
- }
+}
 export default App;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function App(){
-//     let check=false// ture or false
-//     let data={
-//         background:"orange",
-//         color:"white"
-//     }
-//     if(check){
-//   data.background="blue"
-//     }
-//     return(
-//         <>
-//         <h1 style={data}>App Component</h1>
-//         <p>Hello user </p>
-//         </>
-//     )
-// }
-// export default App;
